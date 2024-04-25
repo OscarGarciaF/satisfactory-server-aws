53403fad065020ee40e36c939c024748dd0e5bf5
OscarGarciaF <garcia.oscar1729@gmail.com>
2024-04-24 22:27:08 -0600

    use bucket

--- server-hosting/config.ts
+++ server-hosting/config.ts
@@ -16,7 +16,7 @@ export const Config = {
   // bucket for storing save files
   // you can use an existing bucket
   // or leave it empty to create a new one
-  bucketName: "",
+  bucketName: "conan-hosting-oscar",
   // server hosting vpc
   // Create a vpc and it's id here
   // or leave it empty to use default vpc
